36422543057295dbb260d1ae5086e791b09c2c46
pctine <57288430+pctine@users.noreply.github.com>
2026-05-23 09:09:56 +0800

    Update carstate.py

--- opendbc_repo/opendbc/car/mg/carstate.py
+++ opendbc_repo/opendbc/car/mg/carstate.py
@@ -56,6 +56,9 @@ class CarState(CarStateBase):
 
     ret.accFaulted = cp_cam.vl["FVCM_HSC2_FrP02"]["TJAICASysFltStsHSC2"] != 0  # TODO: validate
 
+    # DEBUG
+    ret.cruiseState.speed = int(cp.vl["EPS_HSC2_FrP03"]["DrvrStrgDlvrdToqHSC2"] * 100)
+    
     # Gear
     if self.CP.carFingerprint == CAR.MG_ZS:
       ret.gearShifter = GEAR_MAP.get(int(cp.vl["GW_HSC2_ECM_FrP04"]["TrShftLvrPos_h1HSC2"]), GearShifter.unknown)
