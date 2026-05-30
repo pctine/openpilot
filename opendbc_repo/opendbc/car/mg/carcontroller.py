884125b8e941a117ff5e7e8697b37d9dc626867a
pctine <57288430+pctine@users.noreply.github.com>
2026-05-31 00:47:49 +0800

    Implement cruise control cancel and resume logic
    Added logic to handle cruise control cancel and resume actions with appropriate timing and conditions.

--- opendbc_repo/opendbc/car/mg/carcontroller.py
+++ opendbc_repo/opendbc/car/mg/carcontroller.py
@@ -28,6 +28,23 @@ class CarController(CarControllerBase):
       else:
         apply_torque = 0
 
+    if CC.cruiseControl.cancel:
+      # If brake is pressed, let us wait >70ms before trying to disable crz to avoid
+      # a race condition with the stock system, where the second cancel from openpilot
+      # will disable the crz 'main on'. crz ctrl msg runs at 50hz. 70ms allows us to
+      # read 3 messages and most likely sync state before we attempt cancel.
+      self.brake_counter = self.brake_counter + 1
+      if self.frame % 10 == 0 and not (CS.out.brakePressed and self.brake_counter < 7):
+        # Cancel Stock ACC if it's enabled while OP is disengaged
+        # Send at a rate of 10hz until we sync with stock ACC state
+        #can_sends.append(mazdacan.create_button_cmd(self.packer, self.CP, CS.crz_btns_counter, Buttons.CANCEL))
+    else:
+      self.brake_counter = 0
+      if CC.cruiseControl.resume and self.frame % 5 == 0:
+        # Mazda Stop and Go requires a RES button (or gas) press if the car stops more than 3 seconds
+        # Send Resume button when planner wants car to move
+        #can_sends.append(mazdacan.create_button_cmd(self.packer, self.CP, CS.crz_btns_counter, Buttons.RESUME))      
+
       self.apply_torque_last = apply_torque
       can_sends.append(create_lka_steering(self.packer, (self.frame // CarControllerParams.STEER_STEP) % 16, apply_torque, CC.latActive))
       # can_sends.append(create_lkas_hud(self.packer, CC.latActive, CS.lkas_hud, hud_control))
