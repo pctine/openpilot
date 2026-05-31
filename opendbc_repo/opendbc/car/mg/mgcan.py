d973132de6ed9bc71cbcb8f5d7a4e3b8206937ac
pctine <57288430+pctine@users.noreply.github.com>
2026-05-31 18:35:26 +0800

    Update mgcan.py

--- opendbc_repo/opendbc/car/mg/mgcan.py
+++ opendbc_repo/opendbc/car/mg/mgcan.py
@@ -1,3 +1,5 @@
+from opendbc.car.mg.values import Buttons
+
 def calc_checksum(values):
   lka_req_toq = values['LKAReqToqHSC2'] + 1024
   lka_req_toq_sts = values['LKAReqToqStsHSC2']
@@ -45,36 +47,37 @@ def create_button_cmd(packer, CP, counter, button):
   can = int(button == Buttons.CANCEL)
   res = int(button == Buttons.RESUME)
 
-  if CP.flags & MazdaFlags.GEN1:
-    values = {
-      "CAN_OFF": can,
-      "CAN_OFF_INV": (can + 1) % 2,
+  values = {
+    "CAN_OFF": can,
+    "CAN_OFF_INV": (can + 1) % 2,
 
-      "SET_P": 0,
-      "SET_P_INV": 1,
+    "SET_P": 0,
+    "SET_P_INV": 1,
 
-      "RES": res,
-      "RES_INV": (res + 1) % 2,
+    "RES": res,
+    "RES_INV": (res + 1) % 2,
 
-      "SET_M": 0,
-      "SET_M_INV": 1,
+    "SET_M": 0,
+    "SET_M_INV": 1,
 
-      "DISTANCE_LESS": 0,
-      "DISTANCE_LESS_INV": 1,
+    "DISTANCE_LESS": 0,
+    "DISTANCE_LESS_INV": 1,
 
-      "DISTANCE_MORE": 0,
-      "DISTANCE_MORE_INV": 1,
+    "DISTANCE_MORE": 0,
+    "DISTANCE_MORE_INV": 1,
 
-      "MODE_X": 0,
-      "MODE_X_INV": 1,
+    "MODE_X": 0,
+    "MODE_X_INV": 1,
 
-      "MODE_Y": 0,
-      "MODE_Y_INV": 1,
+    "MODE_Y": 0,
+    "MODE_Y_INV": 1,
 
-      "BIT1": 1,
-      "BIT2": 1,
-      "BIT3": 1,
-      "CTR": (counter + 1) % 16,
-    }
+    "BIT1": 1,
+    "BIT2": 1,
+    "BIT3": 1,
+    "CTR": (counter + 1) % 16,
+  }
 
-    return packer.make_can_msg("CRZ_BTNS", 0, values)
+  #return packer.make_can_msg("CRZ_BTNS", 0, values)
+  return []
+  
