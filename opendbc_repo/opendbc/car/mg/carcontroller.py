75f4fb73f9d4e311cd24d0e6b5200d76d4ad5810
pctine <57288430+pctine@users.noreply.github.com>
2026-05-31 18:07:48 +0800

    Update carcontroller.py

--- opendbc_repo/opendbc/car/mg/carcontroller.py
+++ opendbc_repo/opendbc/car/mg/carcontroller.py
@@ -3,8 +3,8 @@ from opendbc.can.packer import CANPacker
 from opendbc.car import Bus
 from opendbc.car.lateral import apply_driver_steer_torque_limits
 from opendbc.car.interfaces import CarControllerBase
-from opendbc.car.mg.mgcan import create_lka_steering, create_lkas_hud
-from opendbc.car.mg.values import CarControllerParams
+from opendbc.car.mg import mgcan
+from opendbc.car.mg.values import CarControllerParams, Buttons
 
 
 class CarController(CarControllerBase):
@@ -32,8 +32,8 @@ class CarController(CarControllerBase):
         apply_torque = 0
             
       self.apply_torque_last = apply_torque
-      can_sends.append(create_lka_steering(self.packer, (self.frame // CarControllerParams.STEER_STEP) % 16, apply_torque, CC.latActive))
-      # can_sends.append(create_lkas_hud(self.packer, CC.latActive, CS.lkas_hud, hud_control))
+      can_sends.append(mgcan.create_lka_steering(self.packer, (self.frame // CarControllerParams.STEER_STEP) % 16, apply_torque, CC.latActive))
+      # can_sends.append(mgcan.create_lkas_hud(self.packer, CC.latActive, CS.lkas_hud, hud_control))
 
     # 修正優化：如果 LKA 根本沒激活，確保回傳給 safety 核心的扭力絕對是 0，避免殘留
     if not CC.latActive:
@@ -42,11 +42,8 @@ class CarController(CarControllerBase):
     # Longitudinal control 縱向控制
     if self.CP.openpilotLongitudinalControl:
       accel = float(np.clip(actuators.accel, CarControllerParams.ACCEL_MIN, CarControllerParams.ACCEL_MAX))
-      #can_sends.append(create_longitudinal(self.packer, self.frame, accel, CC.enabled))
-      print(
-        f"[ACCEL]:{actuators.accel:.2f}, "
-        f"[SPEED]:{actuators.speed:.2f}, "
-      )
+      #can_sends.append(mgcan.create_longitudinal(self.packer, self.frame, accel, CC.enabled))
+      print(f"[ACCEL]:{actuators.accel:.2f}")
     else:
       interface_status = None
       if CC.cruiseControl.cancel:
@@ -54,12 +51,15 @@ class CarController(CarControllerBase):
         # send "available" right away as the VDM itself takes a few frames to acknowledge
         interface_status = 1 if self.cancel_frames < 5 else 0
         self.cancel_frames += 1
+        self.brake_counter += 1
       else:
         self.cancel_frames = 0
-
-      #for msg in CS.vdm_adas_status:
-      #  can_sends.append(create_adas_status(self.packer, msg, interface_status))
-    
+        self.brake_counter = 0
+        if CC.cruiseControl.resume and self.frame % 5 == 0:
+          # MG Stop and Go requires a RES button (or gas) press if the car stops more than 3 seconds
+          # Send Resume button when planner wants car to move
+          can_sends.append(mgcan.create_button_cmd(self.packer, self.CP, CS.crz_btns_counter, Buttons.RESUME))
+          
     new_actuators = actuators.as_builder()
     new_actuators.torque = self.apply_torque_last / CarControllerParams.STEER_MAX
     new_actuators.torqueOutputCan = self.apply_torque_last
