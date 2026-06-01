ddca836f130c40f26c4d0807b1c570b52be3e271
pctine <57288430+pctine@users.noreply.github.com>
2026-06-01 13:03:09 +0800

    Update carcontroller.py

--- opendbc_repo/opendbc/car/mg/carcontroller.py
+++ opendbc_repo/opendbc/car/mg/carcontroller.py
@@ -63,12 +63,7 @@ class CarController(CarControllerBase):
       #can_sends.append(mgcan.create_button_cmd(self.packer, CS.crz_btns_counter, Buttons.RESUME))
 
     #CC.cruiseControl.resume = CC.enabled and CS.cruiseState.standstill and not self.sm['longitudinalPlan'].shouldStop
-    print(
-      f'ENABLED={CC.enabled}, '
-      f'STILL={CS.cruiseState.standstill}, '
-      f'RESUME={CC.cruiseControl.resume}, '
-    )
-    
+  
     new_actuators = actuators.as_builder()
     new_actuators.torque = self.apply_torque_last / CarControllerParams.STEER_MAX
     new_actuators.torqueOutputCan = self.apply_torque_last
