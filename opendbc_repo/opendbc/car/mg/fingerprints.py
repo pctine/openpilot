7d2f6cf4dd1db3e0c089edfb033a360a613a2f6d
pctine <57288430+pctine@users.noreply.github.com>
2026-06-04 12:08:47 +0800

    Update fingerprints.py

--- opendbc_repo/opendbc/car/mg/fingerprints.py
+++ opendbc_repo/opendbc/car/mg/fingerprints.py
@@ -26,17 +26,9 @@ FW_VERSIONS = {
       b'\x11\x03\t\x18\x01',
     ],
   },
-  
   CAR.MG_ZS: {
   },
 }
 
-FINGERPRINTS = {
   # TODO: populate via tools/car_porting/auto_fingerprint.py once a route
   # with FW query enabled is captured on the 2025 MG ZS
-  CAR.MG_HS: [{
-    201: 8, 251: 8, 355: 8, 389: 8, 404: 8, 481: 8, 
-    485: 8, 489: 8, 492: 8, 516: 8, 532: 8, 540: 8, 
-    593: 8, 758: 8, 851: 8, 886: 8, 901: 8, 1130: 8, 582: 8
-  }],
-}
