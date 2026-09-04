46e6d89b0d9ce504312f6bed8782a13c8b93a402
pctine <57288430+pctine@users.noreply.github.com>
2026-09-05 00:52:14 +0800

    Introduce FORCE_FW_SCAN for firmware scanning control
    Add FORCE_FW_SCAN flag to control firmware scanning behavior.

--- selfdrive/car/card.py
+++ selfdrive/car/card.py
@@ -91,11 +91,13 @@ class Car:
 
       alpha_long_allowed = self.params.get_bool("AlphaLongitudinalEnabled")
 
+      FORCE_FW_SCAN = True
       cached_params = None
-      cached_params_raw = self.params.get("CarParamsCache")
-      if cached_params_raw is not None:
-        with car.CarParams.from_bytes(cached_params_raw) as _cached_params:
-          cached_params = _cached_params
+      if not FORCE_FW_SCAN:
+        cached_params_raw = self.params.get("CarParamsCache")
+        if cached_params_raw is not None:
+          with car.CarParams.from_bytes(cached_params_raw) as _cached_params:
+            cached_params = _cached_params
 
       self.CI = get_car(*self.can_callbacks, obd_callback(self.params), alpha_long_allowed, is_release, cached_params)
       self.RI = interfaces[self.CI.CP.carFingerprint].RadarInterface(self.CI.CP)
